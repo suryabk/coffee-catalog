fbde612da5b14ddf722f63e7732a21810f7df74c
suryabk <surya27bk@gmail.com>
2023-12-10 18:00:20 +0700

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      pallete: {
+        dark: "#111315",
+        lightDark: "#1B1D1F",
+        grey: "#6F757C",
+        green: "#BEE3CC",
+        light: "#FEF7EE",
+        yellow: "#F6C768",
+        red: "#ED735D",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
