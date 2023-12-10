25865213cfc6b74475f327be0f4a7d0f8db49381
suryabk <surya27bk@gmail.com>
2023-12-10 19:40:11 +0700

    feat: container and heading

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -1,13 +1,13 @@
 function Container() {
   return (
-    <div className="bg-pallete-lightDark mx-auto relative px-12 pt-16 bottom-48 w-4/5 rounded-lg h-screen">
+    <div className="bg-pallete-lightDark mx-auto relative px-4 sm:px-12 pt-16 bottom-48 w-5/6 sm:w-4/5 rounded-lg h-screen overflow-hidden">
       <img
         src="/public/vector.svg"
         alt=""
-        className="absolute left-1/2 top-5 z-0"
+        className="absolute left-1/2 top-5 z-0 "
       />
-      <div className="text-center m-auto sm:w-4/5 md:w-1/2 z-10 relative">
-        <h1 className="text-heading z-10 sm:mb-2 md:mb-4">Our Collection</h1>
+      <div className="text-center m-auto sm:w-1/2 z-10 relative">
+        <h1 className="text-heading z-10 mb-2 sm:mb-4">Our Collection</h1>
         <p className="text-body">
           Introducing our Coffee Collection, a selection of unique coffees from
           different roast types and origins, expertly roasted in small batches
