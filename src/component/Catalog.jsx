590274657c3e9d31170963415215e777cadfef11
suryabk <surya27bk@gmail.com>
2023-12-16 00:16:09 +0700

    feat: wrapper container

--- src/component/Catalog.jsx
+++ src/component/Catalog.jsx
@@ -3,27 +3,52 @@ import Loader from "./Loader";
 import Card from "./Card";
 
 function Catalog() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false)
+  const [coffeeData, setCoffeeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [availableOnly, setAvailableOnly] = useState(false);
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     fetch(
       "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
     )
       .then((response) => response.json())
       .then((res) => {
-        setData(res);
+        setCoffeeData(res);
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
-  console.log(data)
 
   return (
-    
-    <div className="flex flex-wrap justify-center gap-4">
-     {data.map((coffee) => <Card coffee={coffee} key={`coffee-${coffee.id}`} />)}
+    <div className="my-4 flex flex-col gap-4">
+      <div className="flex justify-center gap-4 text-label text-pallete-light">
+        <button
+          type="button"
+          className={availableOnly ? "" : "button-active"}
+          onClick={() => setAvailableOnly(false)}
+        >
+          All Product
+        </button>
+        <button
+          type="button"
+          className={availableOnly ? "button-active" : ""}
+          onClick={() => setAvailableOnly(true)}
+        >
+          Available Now
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {loading && <Loader />}
+        {coffeeData
+          .filter((data) => (availableOnly ? data.available === true : data))
+          .map((coffee) => (
+            <Card coffee={coffee} key={`coffee-${coffee.id}`} />
+          ))}
+      </div>
     </div>
   );
 }
