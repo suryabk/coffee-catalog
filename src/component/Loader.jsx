bad432691edbeecd48978b7fdcd3175a7769babd
--global <surya27bk@gmail.com>
2023-12-19 12:26:38 +0700

    fix: loader style

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -1,9 +1,9 @@
 function Loader() {
   return (
-    <>
+    <div className="my-4 flex flex-col gap-4 justify-center items-center pt-8 sm:pt-12">
       <div className="spinner"></div>
       <span className="text-pallete-yellow animate-pulse">Loading . . .</span>
-    </>
+    </div>
   );
 }
 
